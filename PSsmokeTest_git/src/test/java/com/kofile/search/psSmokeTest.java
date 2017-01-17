13f845a87ef9f7b5f657f5a89ca0034538fdeff9
pashaBerd <pavloberdnyk77@gmail.com>
2017-01-17 19:41:07 +0200

    PS-smoke-test
    12 testcases

---
+++ PSsmokeTest_git/src/test/java/com/kofile/search/psSmokeTest.java
@@ -0,0 +1,161 @@
+package com.kofile.search;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+/**
+ * smoketest_for search.kofile.com
+ */
+
+
+
+public class psSmokeTest {
+
+    @Test(priority = 1)
+    public void Login() {
+        open("http://search.kofile.com/48215");
+        $(".logo>img").shouldBe(visible);
+        $(".button-login").click();
+        $(By.id("dialog-content-holder")).shouldBe(visible);
+        $(By.name("loginusername")).setValue("pavloberdnyk77@gmail.com");
+        $(By.name("loginpassword")).setValue("berdnyk");
+        $(".btn.titleBtnClr").click();
+        $(By.id("userNameLink")).shouldHave(exactText("Uncle Sam")).click();
+        $("#updateuserprofile strong").shouldHave(text("Uncle Sam"));
+        $(By.linkText("Edit Profile")).shouldBe(visible);
+        $(By.linkText("Change Password")).shouldBe(visible);
+        $(By.linkText("Manage Collections")).shouldBe(visible);
+    }
+
+    @Test(priority = 3)
+    public void MarriageSearch() {
+        //System.setProperty("webdriver.chrome.driver", "/Users/pavloberdnyk/IdeaProjects/PublicSearch/chromedriver");
+        open("http://search.kofile.com");
+        $("#searchzone #Department_6").click();
+        $(By.xpath("//H1[text()='Marriage Certificates Search']/self::H1")).shouldBe(visible);
+        $("li #SearchText").setValue("CHANG");
+        $("li .searchbutton").click();
+        $("#results-table span").waitUntil(exist, 10000).shouldHave(text("CHANG"));
+    }
+
+    @Test(priority = 4)
+    public void CattleBrandsSearch() {
+        open("http://search.kofile.com");
+        $("#searchzone #Department_11").click();
+        $(By.xpath("//H1[text()='Cattle Brands Search']/self::H1")).shouldBe(visible);
+        $("li #SearchText").setValue("ADOLFO");
+        $("li .searchbutton").click();
+        $("#results-table span").waitUntil(exist, 10000).shouldHave(text("ADOLFO"));
+    }
+
+    @Test(priority = 5)
+    public void FinancingStatementsSearch() {
+        open("http://search.kofile.com");
+        $("#searchzone #Department_3").click();
+        $(By.xpath("//H1[text()='Financing Statements Search']/self::H1")).shouldBe(visible);
+        $("li #SearchText").setValue("ADAM");
+        $("li .searchbutton").click();
+        $("#results-table span").waitUntil(exist, 10000).shouldHave(text("ADAM"));
+    }
+
+    @Test(priority = 6)
+    public void PlatsSearch() {
+        open("http://search.kofile.com");
+        $("#searchzone #Department_2").click();
+        $(By.xpath("//H1[text()='Plats Search']/self::H1")).shouldBe(visible);
+        $("li #SearchText").setValue("Bros");
+        $("li .searchbutton").click();
+        $("#results-table span").waitUntil(exist, 10000).shouldHave(text("Bros"));
+    }
+
+    @Test(priority = 7)
+    public void ForeclosureSearch() {
+        open("http://search.kofile.com");
+        $("#searchzone #Department_13").click();
+        $(By.xpath("//H1[text()='Foreclosures Search']/self::H1")).shouldBe(visible);
+        $("li #SearchText").setValue("2014-52014");
+        $("li .searchbutton").click();
+        $("#results-table span").waitUntil(exist, 10000).shouldHave(text("2014"));
+    }
+
+    @Test(priority = 2)
+    public void RealPropertySearch() {
+        open("http://search.kofile.com");
+        $("nav #slides-prev-icon").click();
+        $("#searchzone #Department_1").click();
+        $("li #SearchText").setValue("SMITH");
+        $("li .searchbutton").click();
+        $("#results-table span").waitUntil(exist, 10000).shouldHave(text("SMITH"));
+    }
+    @Test(priority = 8)
+    public void AddDocumentToCart() {
+        open("http://search.kofile.com");
+        $("nav #slides-prev-icon").click();
+        $("#searchzone #Department_1").click();
+        $("li #SearchText").setValue("SMITH");
+        $("li .searchbutton").click();
+        $("#results-table span").waitUntil(exist, 10000).shouldHave(text("SMITH"));
+        $(".table.table-striped.dataTable.result-table>tbody>tr:nth-of-type(1)").findElementByCssSelector(".iconCart.icon_table").click();
+        $(By.id("CartItemCount")).shouldHave(exactText("1"));
+        $(By.id("CartDetails")).click();
+        $(By.id("cartHearderBlock-left")).shouldBe(visible);
+        $(".cartItemDetails>td>#cartItemGenericDetails").shouldBe(visible);
+        $(By.id("clearShoppingCartLink")).click();
+    }
+    @Test(priority = 9)
+    public void CheckPreviewPopup() {
+        open("http://search.kofile.com");
+        $("nav #slides-prev-icon").click();
+        $("#searchzone #Department_1").click();
+        $("li #SearchText").setValue("SMITH");
+        $("li .searchbutton").click();
+        $("#results-table span").waitUntil(exist, 10000).shouldHave(text("SMITH")).click();
+        $("#singleImageContainer>div>img").$(by("src","/48215/api/Image/Get/1?docid=1406526&contentId=3263817&version=1&page=1"));
+    }
+    @Test(priority = 10)
+    public void DownloadDocument() {
+        open("http://search.kofile.com");
+        $("#searchzone #Department_1").click();
+        $("li #SearchText").setValue("SMITH");
+        $("li .searchbutton").click();
+        $("#results-table span").waitUntil(exist, 15000).shouldHave(text("SMITH"));
+        $(".table.table-striped.dataTable.result-table>tbody>tr:nth-of-type(1)").findElementByCssSelector(".iconCart.icon_table").click();
+        $(By.id("CartItemCount")).shouldHave(exactText("1"));
+        $(By.id("CartDetails")).click();
+        $( "#delieveryMethod").selectOption("Download");
+        $(By.id("shoppinCartCheckoutBtn")).click();
+        $("#itnet-ec-ordcon-left-block-success-text>span").shouldBe(visible);
+        $(By.id("newSearch")).shouldBe(enabled).click();
+        $(By.id("SearchText")).shouldBe(visible);
+    }
+    @Test(priority = 11)
+    public void EmailDocument() {
+        open("http://search.kofile.com");
+        $("#searchzone #Department_1").click();
+        $("li #SearchText").setValue("SMITH");
+        $("li .searchbutton").click();
+        $("#results-table span").waitUntil(exist, 10000).shouldHave(text("SMITH"));
+        $(".table.table-striped.dataTable.result-table>tbody>tr:nth-of-type(1)").findElementByCssSelector(".iconCart.icon_table").click();
+        $(By.id("CartItemCount")).shouldHave(exactText("1"));
+        $(By.id("CartDetails")).click();
+        $( "#delieveryMethod").selectOption("eMail");
+        $(By.id("shoppinCartCheckoutBtn")).click();
+        $("#itnet-ec-ordcon-left-block-success-text>span").shouldBe(visible);
+        $(By.id("newSearch")).shouldBe(enabled).click();
+        $(By.id("SearchText")).shouldBe(visible);
+    }
+    @Test(priority = 12)
+    public void Logout() {
+        $(By.id("signOutLink")).click();
+        $(By.id("logoutOk")).click();
+        $(By.id("registerUser")).shouldBe(visible);
+
+    }
+}
+
+
